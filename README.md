8d909529663a642fad6fd476cb64c2faf77af7c7
zeosjb <jose.benitez@alumnos.ucn.cl>
2023-12-13 15:46:24 -0300

    Feat: Modal, Delete

---
+++ README.md
@@ -0,0 +1,23 @@
+# Prueba 3 - Introducción al desarrollo web móvil
+
+## Requisitos previos
+
+1. **Node.js:**
+    #### Asegúrate de tener instalado [Node.js](https://nodejs.org/) en tu máquina.
+
+## Uso Frontend
+
+1. **Navegar hacia el frontend:**
+
+    ```bash
+    cd mobile
+
+2. **Instalar dependencias:**
+
+    ```bash
+    npm  install
+
+3. **Para iniciar realiza:**
+
+    ```bash
+    npx expo start --web
--- mobile/App.js
+++ mobile/App.js
@@ -1,53 +1,107 @@
-import React, { useState, useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import { PaperProvider, Card, Text } from 'react-native-paper';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Modal as RNWModal } from "react-native";
+import { PaperProvider, Card, Text, Button } from "react-native-paper";
+import axios from "axios";
 
 export default function App() {
   const [posts, setPosts] = useState([]);
   const [comments, setComments] = useState([]);
+  const [selectedPostId, setSelectedPostId] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
-    axios.get('http://localhost:3000/posts')
-      .then(response => {
+    axios
+      .get("http://localhost:3000/posts")
+      .then((response) => {
         setPosts(response.data);
       })
-      .catch(error => {
-        alert('Error fetching posts:', error);
+      .catch((error) => {
+        alert("Error fetching posts:", error);
       });
 
-    axios.get('http://localhost:3000/comments')
-      .then(response => {
+    axios
+      .get("http://localhost:3000/comments")
+      .then((response) => {
         setComments(response.data);
       })
-      .catch(error => {
-        alert('Error fetching comments:', error);
+      .catch((error) => {
+        alert("Error fetching comments:", error);
       });
   }, []);
 
   const getCommentsForPost = (postId) => {
-    return comments.filter(comment => comment.postId === postId);
+    return comments.filter((comment) => comment.postId === postId);
+  };
+
+  const showModal = (postId) => {
+    setSelectedPostId(postId);
+    setModalVisible(true);
+  };
+
+  const hideModal = () => {
+    setSelectedPostId(null);
+    setModalVisible(false);
+  };
+
+  const handleDeletePost = async () => {
+    try {
+      await axios.delete(`http://localhost:3000/posts/${selectedPostId}`);
+      setPosts((prevPosts) =>
+        prevPosts.filter((post) => post.id !== selectedPostId)
+      );
+      alert(`Delete post with ID ${selectedPostId}`);
+    } catch (error) {
+      alert(`Error deleting post with ID ${selectedPostId}: ${error.message}`);
+    } finally {
+      hideModal();
+    }
   };
 
   return (
     <PaperProvider>
       <View style={styles.container}>
         <Text style={styles.textView}>List of Posts</Text>
-        {posts.map(post => (
+        {posts.map((post) => (
           <Card key={post.id} style={styles.cardView}>
-            <Card.Cover source={post.image} />
+            <Card.Cover source={{ uri: post.image }} style={styles.cardCover} />
             <Card.Title
               title={post.title}
               subtitle={`Author: ${post.author}`}
             />
             <Card.Content>
-              {getCommentsForPost(post.id).map(comment => (
-                <Text key={comment.id} style={styles.commentView}>{comment.body}</Text>
+              {getCommentsForPost(post.id).map((comment) => (
+                <Text key={comment.id} style={styles.commentView}>
+                  {comment.body}
+                </Text>
               ))}
+              <Button
+                icon="delete"
+                mode="outlined"
+                style={styles.buttonView}
+                onPress={() => showModal(post.id)}
+                color="#FF0000"
+              >
+                Delete
+              </Button>
             </Card.Content>
           </Card>
         ))}
+        <RNWModal
+          visible={modalVisible}
+          onRequestClose={hideModal}
+          transparent
+        >
+          <View style={styles.modalContainer}>
+            <Text style={styles.modalText}>
+              Are you sure you want to delete this post?
+            </Text>
+            <Button onPress={handleDeletePost} color="#FF0000">
+              Yes, Delete
+            </Button>
+            <Button onPress={hideModal}>Cancel</Button>
+          </View>
+        </RNWModal>
         <StatusBar style="auto" />
       </View>
     </PaperProvider>
@@ -57,27 +111,58 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#0096ff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   cardView: {
-    width: '95%',
+    width: "95%",
     marginVertical: 5,
     paddingVertical: 10,
     paddingHorizontal: 5,
+    borderRadius: 10,
+    elevation: 3,
+  },
+  cardCover: {
+    height: 400,
+    resizeMode: "cover",
+  },
+  infoView: {
+    color: "black",
+    fontSize: 16,
+    fontWeight: "bold",
   },
   textView: {
-    color: 'black',
+    color: "white",
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
     marginTop: 5,
   },
   commentView: {
-    backgroundColor: 'gray',
+    borderWidth: 2,
+    borderColor: "gray",
     padding: 5,
-    borderRadius: 8,
-    marginBottom: 4
-  }
+    borderRadius: 5,
+    marginBottom: 4,
+    fontSize: 14,
+  },
+  buttonView: {
+    marginTop: 5,
+  },
+  modalContainer: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContent: {
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 10,
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
 });
