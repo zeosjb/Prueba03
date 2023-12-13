3f953bdb0a6f60934c25b782d4f6ef1c0798b71b
zeosjb <jose.benitez@alumnos.ucn.cl>
2023-12-13 15:15:19 -0300

    Fix: App, View of the post

--- mobile/App.js
+++ mobile/App.js
@@ -1,25 +1,53 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import { PaperProvider, Avatar, Button, Card, Text } from "react-native-paper";
-
-
+import React, { useState, useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import { PaperProvider, Card, Text } from 'react-native-paper';
+import axios from 'axios';
 
 export default function App() {
+  const [posts, setPosts] = useState([]);
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/posts')
+      .then(response => {
+        setPosts(response.data);
+      })
+      .catch(error => {
+        alert('Error fetching posts:', error);
+      });
+
+    axios.get('http://localhost:3000/comments')
+      .then(response => {
+        setComments(response.data);
+      })
+      .catch(error => {
+        alert('Error fetching comments:', error);
+      });
+  }, []);
+
+  const getCommentsForPost = (postId) => {
+    return comments.filter(comment => comment.postId === postId);
+  };
+
   return (
     <PaperProvider>
       <View style={styles.container}>
-        <Text style={styles.textView}>List of Post</Text>
-        <Card style={styles.cardView}>
-          <Card.Title
-            title="Card Title"
-            subtitle="Card Subtitle"
-          />
-          <Card.Content>
-            <Text variant="titleLarge">Card title</Text>
-            <Text variant="bodyMedium">Card content</Text>
-          </Card.Content>
-          <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
-        </Card>
+        <Text style={styles.textView}>List of Posts</Text>
+        {posts.map(post => (
+          <Card key={post.id} style={styles.cardView}>
+            <Card.Cover source={post.image} />
+            <Card.Title
+              title={post.title}
+              subtitle={`Author: ${post.author}`}
+            />
+            <Card.Content>
+              {getCommentsForPost(post.id).map(comment => (
+                <Text key={comment.id}>{comment.body}</Text>
+              ))}
+            </Card.Content>
+          </Card>
+        ))}
         <StatusBar style="auto" />
       </View>
     </PaperProvider>
@@ -29,18 +57,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   cardView: {
-    width: '100%',
+    width: '95%',
+    marginVertical: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 5,
   },
   textView: {
     color: 'black',
     fontSize: 18,
-    fontVariant: 'bold',
+    fontWeight: 'bold',
     marginBottom: 10,
-    marginTop: 0
-  }
+    marginTop: 0,
+  },
 });
